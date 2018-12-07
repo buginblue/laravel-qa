6204e7bab1a519493d82231cb7546e99b089b9bb
Mark Livermore <mlivermore@aol.com>
2018-12-07 11:59:57 +0000

    Started show too soon

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">
+                <div class="card-title">
                     <div class="d-flex align-items-center">
                         <h1>{{ $question->title }}</h1>
                         <div class="ml-auto">
@@ -15,8 +15,34 @@
                     
                 </div>
 
+                <div class="media">
+                    <div class="media-body">                        
+                        {!! $question->body_html !!}
+                        <div class="float-right">
+                            <span class="text-muted">Answered {{ $question->created_date }}</span>
+                            <div class="media mt-2">
+                                <a href="{{ $question->user->url }}" class="pr-2">
+                                    <img src="{{ $question->user->avatar }}">
+                                </a>
+                                <div class="media-body mt-1">
+                                    <a href="{{ $question->user->url }}">{{ $question-> }}</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row mt-4">
+        <div class="col-md-12">
+            <div  class="card">
                 <div class="card-body">
-                    {!! $question->body_html !!}
+                    <div class="card-title">
+                        <h2>{{ $question->answers_count . " " . str_plural('Answer') }}</h2>
+                    </div>
+                    <hr>
+                    
                 </div>
             </div>
         </div>
